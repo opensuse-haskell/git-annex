opensuse-haskell/git-annex

commit 8693eda88a360dd5f1931b43f49720d2cb4e1aeb

split local branch updates between push and pull commands
Before both push and pull did all local git branch updates the same as
sync, despite not being documented to behave that way.

Command/Sync.hs
@@ -334,7 +334,7 @@ seek' o = startConcurrency transferStages $ do
 						, [ commitAnnex, mergeAnnex ]
 						]
 	
-			void $ includeCommandAction $ withbranch $ pushLocal o
+			void $ includeCommandAction $ withbranch $ updateLocal o
 			-- Pushes to remotes can run concurrently.
 			mapM_ (commandAction . withbranch . pushRemote o) gitremotes
   where
@@ -439,8 +439,10 @@ commitMsg = do
 		++ maybe "unknown" fromUUIDDesc (M.lookup u m)
 
 mergeLocal :: [Git.Merge.MergeConfig] -> SyncOptions -> CurrBranch -> CommandStart
-mergeLocal mergeconfig o currbranch = stopUnless (notOnlyAnnex o) $
+mergeLocal mergeconfig o currbranch = stopUnless shouldmerge $
 	mergeLocal' mergeconfig o currbranch
+  where
+	shouldmerge = pure (pullOption o) <&&> notOnlyAnnex o
 
 mergeLocal' :: [Git.Merge.MergeConfig] -> SyncOptions -> CurrBranch -> CommandStart
 mergeLocal' mergeconfig o currbranch@(Just branch, _) =
@@ -494,46 +496,50 @@ needMerge currbranch headbranch
 		let branch' = maybe headbranch (adjBranch . originalToAdjusted headbranch) madj
 		in hassyncbranch <&&> inRepo (Git.Branch.changed branch' syncbranch)
 
-pushLocal :: SyncOptions -> CurrBranch -> CommandStart
-pushLocal o b = stopUnless (notOnlyAnnex o) $ do
-	updateBranches b
+updateLocal :: SyncOptions -> CurrBranch -> CommandStart
+updateLocal o b = stopUnless (notOnlyAnnex o) $ do
+	updateBranches (pullOption o) (pushOption o) b
 	stop
 
-updateBranches :: CurrBranch -> Annex ()
-updateBranches (Nothing, _) = noop
-updateBranches (Just branch, madj) = do
-	-- When in a view branch, update it to reflect any changes
-	-- of its parent branch or the metadata.
+updateBranches :: Bool -> Bool -> CurrBranch -> Annex ()
+updateBranches _ _ (Nothing, _) = noop
+updateBranches forpull forpush (Just branch, madj) = do
 	currentView >>= \case
-		Just (view, madj') -> updateView view madj' >>= \case
-			Nothing -> noop
-			Just newcommit -> do
-				ok <- inRepo $ Git.Command.runBool
-					[ Param "merge"
-					, Param (Git.fromRef newcommit)
-					]
-				unless ok $
-					giveup $ "failed to update view"
-				case madj' of
-					Nothing -> noop
-					Just adj -> updateadjustedbranch adj
+		Just (view, madj')
+			| forpull -> updateview view madj'
+			| otherwise -> noop
 		-- When in an adjusted branch, propagate any changes
 		-- made to it back to the original branch.
 		Nothing -> case madj of
 			Just adj -> do
-				propigateAdjustedCommits branch adj
-				updateadjustedbranch adj
+				when forpush $
+					propigateAdjustedCommits branch adj
+				when forpull $
+					updateadjustedbranch adj
 			Nothing -> noop
 	
 	-- Update the sync branch to match the new state of the branch
 	inRepo $ updateBranch (syncBranch branch) (fromViewBranch branch)
   where
-	-- The adjusted branch may also need to be updated, if the adjustment
+	-- The adjusted branch may need to be updated, if the adjustment
 	-- is not stable, and the usual configuration does not update it.
 	updateadjustedbranch adj = unless (adjustmentIsStable adj) $
 		annexAdjustedBranchRefresh <$> Annex.getGitConfig >>= \case
 			0 -> adjustedBranchRefreshFull adj branch
 			_ -> return ()
+	
+	updateview view madj' = updateView view madj' >>= \case
+		Nothing -> noop
+		Just newcommit -> do
+			ok <- inRepo $ Git.Command.runBool
+				[ Param "merge"
+				, Param (Git.fromRef newcommit)
+				]
+			unless ok $
+				giveup $ "failed to update view"
+			case madj' of
+				Nothing -> noop
+				Just adj -> updateadjustedbranch adj
 
 updateBranch :: Git.Branch -> Git.Branch -> Git.Repo -> IO ()
 updateBranch syncbranch updateto g = 

Assistant/Threads/Committer.hs
@@ -253,7 +253,7 @@ commitStaged msg = do
 				, Param msg
 				]
 			when ok $
-				Command.Sync.updateBranches =<< getCurrentBranch
+				Command.Sync.updateBranches True True =<< getCurrentBranch
 			{- Commit the git-annex branch. This comes after
 			 - the commit of the staged changes, so that
 			 - annex.commitmessage-command can examine that

doc/todo/add_--json-progress_support_in_push_and_pull/comment_9_41c737bc7488fc440e07b53504caf06b._comment
@@ -1,0 +1,16 @@
+[[!comment format=mdwn
+ username="joey"
+ subject="""comment 9"""
+ date="2026-05-29T13:49:46Z"
+ content="""
+FWIW, I've split updateBranches between pull and push now.
+
+On `git-annex push` all it does is propagate adjusted branches
+changes back to the original branch.
+
+On `git-annex pull` it handles updating the view branch and/or
+propagating changes from the original branch to the adjusted branch.
+
+Also, `git-annex push` was fixed to not merge synced/master into master
+and to not update the adjusted branch when the original branch has changed.
+"""]]

commit 0f045b7ca78a136d51bdf027fa893a0c9b6547aa

update branches in sync mode
8693eda88a360dd5f1931b43f49720d2cb4e1aeb broke the edge case of
git-annex sync --no-push --no-pull --no-content,
which commits and propigates an adjusted branch back to the original
branch.

Command/Sync.hs
@@ -498,8 +498,10 @@ needMerge currbranch headbranch
 
 updateLocal :: SyncOptions -> CurrBranch -> CommandStart
 updateLocal o b = stopUnless (notOnlyAnnex o) $ do
-	updateBranches (pullOption o) (pushOption o) b
+	updateBranches (pullOption o || syncmode) (pushOption o || syncmode) b
 	stop
+  where
+	syncmode = operationMode o == SyncMode
 
 updateBranches :: Bool -> Bool -> CurrBranch -> Annex ()
 updateBranches _ _ (Nothing, _) = noop